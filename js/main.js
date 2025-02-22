15042d768c6f48762c5fc409d60e53eb12bb8ed1
Brian Ruiz Moreno <brian.ruizmoreno2311@gmail.com>
2025-02-22 01:20:35 -0300

    actualiza y añade funciones

--- js/main.js
+++ js/main.js
@@ -1,90 +1,192 @@
-const listaNombres = [];
+// Se declara listaNombres de forma global para poder manipularla en todas las funciones
+let listaNombres = JSON.parse(localStorage.getItem('listaNombres')) || [];
 
+//Se crea una función para agregar nombres a la lista
 const agregarNombres = () => {
-    const nombre = prompt('Ingrese un nombre en mayúsculas:');
+    
+    let nombre = prompt('Ingrese un nombre:');
 
     if (nombre === null) {
-        alert('No ha ingresado ningún nombre.');
+        opcionesMenu();
         return;
     }
-
-    if (nombre === "") {
+    //Se valida que el dato ingresado no sea un número
+    let esNumerico = false;
+    for (let i = 0; i < nombre.length; i++) {
+        if (!isNaN(nombre[i]) && nombre[i] !== " ") {
+            esNumerico = true;
+            break; 
+        }
+    }
+    //Se valida que el usuario haya ingresado un nombre válido
+    if (nombre === "" || esNumerico) {
         alert('Por favor, ingrese un nombre válido.');
         return agregarNombres();
     } else {
-        nombre.toUpperCase();
-        if (listaNombres.includes(nombre)) {
-            alert('El nombre ya se ha ingresado, por favor ingrese otro.');
+        //Se valida que el nombre no se haya ingresado previamente
+        if (listaNombres.some(el => el.nombre === nombre)) {
+            alert(`El nombre "${nombre}" ya se ha ingresado, por favor ingrese otro.`);
             return agregarOtroNombre();
         }
-        listaNombres.push(nombre);
+
+        let edad = ingresarEdad();
+        //Se agrega el nombre a la lista
+        listaNombres.push({nombre, edad});
+        //Se guarda la lista en el localStorage
+        localStorage.setItem('listaNombres', JSON.stringify(listaNombres));
+        alert(`El nombre "${nombre}" de "${edad} ${edad > 1 ? 'años' : 'año'}" ha sido agregado a la lista.`);
         agregarOtroNombre();        
     }
 }
-const agregarOtroNombre = () =>{
-    const continuar = prompt('¿Desea agregar otro nombre?').toLowerCase();
-        if (continuar === 'no') {
-            return;
-        } else if (continuar === 'si') {
-            agregarNombres();
-        } else {
-            alert('Opción inválida, ingrese "si" o "no".');
-            return agregarOtroNombre();
-        }
+//Se crea una función para ingresar la edad
+const ingresarEdad = () => {
+
+    let edad = parseInt(prompt('Ingrese la edad del nombre ingresado:'));
+    //Se valida que el dato ingresado sea un número
+    if (!edad || isNaN(edad) || edad <= 0 || edad >= 110){
+        alert('Por favor, ingrese una edad válida.');
+        return ingresarEdad();
+    }
+
+    return edad;
 }
+
+//Se pregunta al usuario si desea agregar otro nombre
+const agregarOtroNombre = () =>{
+        if (confirm('¿Desea agregar otro nombre?')) {
+            agregarNombres();} else{
+            opcionesMenu();
+            }
+}
+
+//Se crea una función para ordenar los nombres de forma ascendente
 const ordenarAscendente = () => {
-    if (listaNombres.length === 0) {
-        alert('No hay nombres para ordenar');
-        return;
+    if (JSON.parse(localStorage.getItem('ordenarAscendente')) === 1) {
+        if (listaNombres.length === 0) {
+            alert('No hay nombres para ordenar');
+            return;
+        } else {
+            listaNombres.sort((a, b) => a.nombre.localeCompare(b.nombre));
+            alert(`Lista ordenada alfabéticamente de forma 
+                ascendentemente:\n\n${listaNombres.map(el => el.nombre).join('\n')}`);
+                localStorage.removeItem('ordenarAscendente');
+            return listaNombres;
+        }
     } else {
-        listaNombres.sort();
-        alert(`La lista de nombres ordenada de forma ascendente es: ${'\n\n' + listaNombres.join('\n')}`);
-        return listaNombres;
+        listaNombres.sort((a, b) => a.edad - b.edad);
+            alert(`Lista ordenada por edades de forma 
+                ascendentemente:\n\n${listaNombres.map(el => el.edad).join('\n')}`);
+                localStorage.removeItem('ordenarAscendente');
+            return listaNombres;
     }
 };
 
+//Se crea una función para ordenar los nombres de forma descendente
 const ordenarDescendente = () => {
-    if (listaNombres.length === 0) {
-        alert('No hay nombres para ordenar.');
-        return;
+    if (JSON.parse(localStorage.getItem('ordenarDescendente')) === 1) {
+        if (listaNombres.length === 0) {
+            alert('No hay nombres para ordenar');
+            return;
+        } else {
+            listaNombres.sort((a, b) => b.nombre.localeCompare(a.nombre));
+            alert(`Lista ordenada alfabéticamente de forma 
+                descendentemente:\n\n${listaNombres.map(el => el.nombre).join('\n')}`);
+                localStorage.removeItem('ordenarDescendente');
+            return listaNombres;
+        }
     } else {
-        listaNombres.sort().reverse();
-        alert(`La lista de nombres ordenada de forma descendente es: ${'\n\n' + listaNombres.join('\n')}`);
-        return listaNombres;
+        listaNombres.sort((a, b) => b.edad - a.edad);
+            alert(`Lista ordenada por edades de forma 
+                descendentemente:\n\n${listaNombres.map(el => el.edad).join('\n')}`);
+                localStorage.removeItem('ordenarDescendente');
+            return listaNombres;
     }
 };
 
+//Se crea una función para realizar un sorteo
 const sorteo = () => {
     if (listaNombres.length === 0) {
         alert('No hay nombres para sortear.');
         return;
     } else {
-        const indiceGanador = Math.floor(Math.random() * listaNombres.length);
-        const nombreGanador = listaNombres[indiceGanador];
-        alert(`El nombre ganador es: ${'\n\n' + nombreGanador}`);
-        return nombreGanador;
+        const ganador = listaNombres[Math.floor(Math.random() * listaNombres.length)];
+        alert(`El nombre ganador es: ${'\n\n' + ganador.nombre} de ${ganador.edad} ${ganador.edad > 1 ? 'años' : 'año'}`);
+        return ganador;
     }
 };
 
+//Se crea una función para reiniciar la lista de nombres
+const reiniciar = () => {
+    listaNombres.splice(0, listaNombres.length);
+    localStorage.removeItem('listaNombres');
+    alert('La lista de nombres ha sido reiniciada.');
+    return listaNombres;
+}
+//Se crea una función para mostrar las opciones del menú
 const opcionesMenu = () => {
     let opcionUsuario;
+    let opcionMenu;
+    let opcionOrdenar;
     do {
     opcionUsuario = parseInt(prompt(`¿Qué desea hacer con los nombres? \n
         1 - Ordenar ascendentemente. \n
         2 - Ordenar descendentemente. \n
         3 - Realizar un sorteo. \n
-        4 - Agregar otro nombre. \n\n
+        4 - Agregar otro nombre. \n
+        5 - Reiniciar la lista de nombres. \n
         0 - Salir.`));
 
     switch (opcionUsuario) {
         case 0:
             alert('¡Gracias por utilizar el programa!');
-            break;
+            return;
         case 1:
-            ordenarAscendente();
+            opcionMenu = parseInt(prompt(`¿Por nombres o por edades? \n
+                1 - Por nombres. \n
+                2 - Por edades.\n\n 
+                0 - Volver al menú principal.`));
+                switch (opcionMenu) {
+                    case 0:
+                        opcionesMenu();
+                        break;
+                    case 1:
+                        opcionOrdenar = 1;
+                        localStorage.setItem('ordenarAscendente', JSON.stringify(opcionOrdenar));
+                        ordenarAscendente();
+                        break;
+                    case 2:
+                        opcionOrdenar = 2;
+                        localStorage.setItem('ordenarAscendente', JSON.stringify(opcionOrdenar));
+                        ordenarAscendente();
+                        break;
+                    default:
+                        alert('Opción inválida. Por favor, vuelva a intentarlo.');
+                        break;
+                }
             break;
         case 2:
-            ordenarDescendente();
+            opcionMenu = parseInt(prompt(`¿Por nombres o por edades? \n
+                1 - Por nombres. \n
+                2 - Por edades.\n\n 
+                0 - Volver al menú principal.`));
+                switch (opcionMenu) {
+                    case 0:
+                        opcionesMenu();
+                        break;
+                    case 1:
+                        opcionOrdenar = 1;
+                        localStorage.setItem('ordenarDescendente', JSON.stringify(opcionOrdenar));
+                        ordenarDescendente();
+                        break;
+                    case 2:
+                        opcionOrdenar = 2;
+                        localStorage.setItem('ordenarDescendente', JSON.stringify(opcionOrdenar));
+                        ordenarDescendente();
+                        break;
+                    default:
+                        alert('Opción inválida. Por favor, vuelva a intentarlo.');
+                        break;
+                }
             break;
         case 3:
             sorteo();
@@ -92,13 +194,17 @@ const opcionesMenu = () => {
         case 4:
             agregarNombres();
             break;
+        case 5:
+            reiniciar();
+            break;
         default:
             alert(`Opción inválida, ingrese un número correcto.`);
             opcionesMenu();
             break;
      } 
-    } while (opcionUsuario !==0)
+    } 
+    while (opcionUsuario !== 0);
+    return;
 }
-
+//Se inicia el programa
 agregarNombres();
-opcionesMenu();
